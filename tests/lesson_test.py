8c2c23e2dc9d06cdc95820349c02505940d0d43e
Helen Langridge <langridge.helen@outlook.com>
2022-08-20 15:27:26 +0100

    lesson tests complete

--- tests/lesson_test.py
+++ tests/lesson_test.py
@@ -8,4 +8,25 @@ class TestLesson(unittest.TestCase):
         self.lesson2 = Lesson("Peaceful Yoga", "Wednesday 7th September", "17:30", "60 mins", "Clara", "Studio", 5, "Stretch off the stresses of mid-week with our restorative yoga hour")
 
     def test_lesson1_has_title(self):
-        
+        self.assertEqual("Spin with Jim", self.lesson1.title)
+
+    def test_lesson2_has_date(self):
+        self.assertEqual("Wednesday 7th September", self.lesson2.date)
+
+    def test_lesson1_has_time(self):
+        self.assertEqual("07:00", self.lesson1.time)
+
+    def test_lesson2_has_duration(self):
+        self.assertEqual("60 mins", self.lesson2.duration)
+
+    def test_lesson1_has_instructor_string(self):
+        self.assertEqual("Jim", self.lesson1.instructor)
+
+    def test_lesson2_has_location_string(self):
+        self.assertEqual("Studio", self.lesson2.location)
+
+    def test_lesson1_has_capacity(self):
+        self.assertEqual(20, self.lesson1.capacity)
+
+    def test_lesson2_has_description(self):
+        self.assertEqual("Stretch off the stresses of mid-week with our restorative yoga hour", self.lesson2.description)
