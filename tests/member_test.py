819d50df1ac416aae9ad91ce0c385a367d3e2518
Helen Langridge <langridge.helen@outlook.com>
2022-08-20 16:14:49 +0100

    integrated sql test working, added gym member

--- tests/member_test.py
+++ tests/member_test.py
@@ -1,5 +1,8 @@
 import unittest
 from models.member import Member
+import repositories.member_repository as member_repository
+from repositories.member_repository import *
+from db.run_sql import run_sql
 
 class TestMember(unittest.TestCase):
 
@@ -9,3 +12,11 @@ class TestMember(unittest.TestCase):
 
     def test_member1_has_name(self):
         self.assertEqual("Mike Langridge", self.member1.name)
+
+    def test_member_added_to_db(self):
+        member = Member("Matt Thomson")
+        member_repository.save(member)
+        sql = "SELECT name FROM members WHERE id = %s"
+        values = [member.id]
+        results = run_sql(sql,values)
+        self.assertEqual("Matt Thomson", results[0]['name'])
