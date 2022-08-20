f0d786750505defebd41b82aa7c9abd02e26ed80
Helen Langridge <langridge.helen@outlook.com>
2022-08-20 15:18:38 +0100

    member test passed and add duration to lesson

--- tests/lesson_test.py
+++ tests/lesson_test.py
@@ -6,3 +6,6 @@ class TestLesson(unittest.TestCase):
     def setUp(self):
         self.lesson1 = Lesson("Spin with Jim", "Monday 5th September", "07:00", "60 mins", "Jim", "Spin Room", 20, "Rise and shine with an hour's intense spin class that'll have you hitting your VO2 max over and over again. Don't forget your own electrolyte drink and towel")
         self.lesson2 = Lesson("Peaceful Yoga", "Wednesday 7th September", "17:30", "60 mins", "Clara", "Studio", 5, "Stretch off the stresses of mid-week with our restorative yoga hour")
+
+    def test_lesson1_has_title(self):
+        
--- tests/member_test.py
+++ tests/member_test.py
@@ -3,6 +3,9 @@ from models.member import Member
 
 class TestMember(unittest.TestCase):
 
-    def SetUp(self):
+    def setUp(self):
         self.member1 = Member("Mike Langridge")
         self.member2 = Member("Helen Langridge")
+
+    def test_member1_has_name(self):
+        self.assertEqual("Mike Langridge", self.member1.name)
