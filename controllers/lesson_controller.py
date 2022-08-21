1a1760a7b0ef8a2ffc770dfa0890aeae2867c54d
Helen Langridge <langridge.helen@outlook.com>
2022-08-21 18:38:04 +0100

    add class complete

--- controllers/lesson_controller.py
+++ controllers/lesson_controller.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, Blueprint
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
 import repositories.lesson_repository as lesson_repository
-import repositories.member_repository as member_repository
+from models.lesson import Lesson 
 
 lessons_blueprint = Blueprint("classes", __name__)
 
@@ -14,3 +15,17 @@ def view_one(id):
     lesson = lesson_repository.select(id)
     members = lesson_repository.get_member_list_for_lesson(lesson)
     return render_template("lessons/onelesson.html", lesson = lesson, members = members)
+
+@lessons_blueprint.route("/classes", methods=['POST'])
+def add_lesson():
+    title = request.form['title']
+    date = request.form['date']
+    time = request.form['time']
+    duration = request.form['duration']
+    instructor = request.form['instructor']
+    location = request.form['location']
+    capacity = request.form['capacity']
+    description = request.form['description']
+    new_lesson = Lesson(title, date, time, duration, instructor, location, capacity, description)
+    lesson_repository.save(new_lesson)
+    return redirect("/classes")
