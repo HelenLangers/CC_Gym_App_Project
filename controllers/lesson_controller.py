7a13037209aabe650cfc2c43129c1ddcf6f618ff
Helen Langridge <langridge.helen@outlook.com>
2022-08-21 15:12:39 +0100

    moved main and section out of base.html

--- controllers/lesson_controller.py
+++ controllers/lesson_controller.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, Blueprint
 import repositories.lesson_repository as lesson_repository
 
-lessons_blueprint = Blueprint("lessons", __name__)
+lessons_blueprint = Blueprint("classes", __name__)
 
+@lessons_blueprint.route("/classes")
+def locations():
+    lessons = lesson_repository.select_all()
+    return render_template("/classes/classes.html", lessons = lessons)
